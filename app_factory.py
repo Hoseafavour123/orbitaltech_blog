c859632cbc406f338215e9603b38f155008a6d72
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-10 02:42:56 +0100

    Loginmanager cleanup

--- app_factory.py
+++ app_factory.py
@@ -5,12 +5,6 @@ from flask_ckeditor import CKEditor
 import os
 
 login_manager = LoginManager()
-"""Keep current user loaded in session"""
-from models import User
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
 login_manager.login_message_category = "info"
@@ -32,9 +26,14 @@ def create_app():
     app.config["UPLOAD_FOLDER"] = "static/images/"
     #app.config["UPLOAD_FOLDER"] = "orbitaltech_blog/static/images"
     
-    login_manager.init_app(app)
-    ckeditor.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    """Keep current user loaded in session"""
+    from models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    ckeditor.init_app(app)
     
     # Register blueprints
     from auth import auth
