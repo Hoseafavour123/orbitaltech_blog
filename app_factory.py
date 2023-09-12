2de264cd73971eb13f4bbb1349889cab4a9a80ef
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-12 21:24:14 +0100

    removed sessioning

--- app_factory.py
+++ app_factory.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import  LoginManager
 from flask_ckeditor import CKEditor
-from flask_session import Session
 import os
 
 login_manager = LoginManager()
@@ -11,7 +10,6 @@ login_manager.login_view = "auth.login"
 login_manager.login_message_category = "info"
 
 db = SQLAlchemy()
-sess = Session()
 ckeditor = CKEditor()
 
 # Application factory
@@ -19,8 +17,6 @@ def create_app():
     app = Flask(__name__)
     
     app.config["SECRET_KEY"] = "secret"
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
     #app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/prime_db"
     #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mtxhfipnrdhayb:3050c1a2c47bb7cb33f36d031b12e74a69d200281de4c6eddebe78d951e54cf8@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d3p9qp8g4p4otu"
@@ -37,7 +33,6 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
     ckeditor.init_app(app)
-    sess.init_app(app)
     
     # Register blueprints
     from auth import auth
