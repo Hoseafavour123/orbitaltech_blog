57af45afe611ba3b9722fa778018c7e0d2af69e5
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-13 17:36:38 +0100

    Placed login mager and with block within create app

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ from models import User, Post
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        # Database migration
-        migrate = Migrate(app, db)
     app.run()
