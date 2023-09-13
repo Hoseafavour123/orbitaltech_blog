ec7fab0afa3fefda55a75fde9f9ebe23bef6f244
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-13 17:01:10 +0100

    Moved with block

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ from models import User, Post
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    # Database migration
-    migrate = Migrate(app, db)
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        # Database migration
+        migrate = Migrate(app, db)
     app.run()
