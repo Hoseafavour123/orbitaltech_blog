b041e82d8a4ab5bf93ecdb890122fb50080e10b3
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-13 04:44:25 +0100

    Fixed loginmanager import error

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, redirect, render_template, flash, url_for
 from models import User
 from forms import LoginForm, SignUpForm
-from app_factory import login_manager, db
+from app_factory import db
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
