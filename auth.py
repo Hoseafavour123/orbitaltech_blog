a56da8e7e86d6fbd07ac360c754daf3dafb331ae
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-13 18:00:39 +0100

    Set remember to true for login

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, redirect, render_template, flash, url_for
 from models import User
 from forms import LoginForm, SignUpForm
 from app_factory import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -17,7 +17,7 @@ def login():
         try:
             user = User.query.filter_by(email=form.email.data).first()
             if check_password_hash(user.password, form.password.data):
-                login_user(user)
+                login_user(user, remember=True)
                 return redirect(url_for("views.home"))
             else:
                 flash("Invalid password!", "danger")
