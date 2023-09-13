f0113aef57459e3eee9886432b129f5416245064
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-13 17:19:27 +0100

    Properly configure login manager

--- app_factory.py
+++ app_factory.py
@@ -27,14 +27,14 @@ def create_app():
     #app.config["UPLOAD_FOLDER"] = "orbitaltech_blog/static/images"
     
     db.init_app(app)
+    login_manager.init_app(app)
+    ckeditor.init_app(app)
     
     """Keep current user loaded in session"""
     from models import User
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    login_manager.init_app(app)
-    ckeditor.init_app(app)
     
     # Register blueprints
     from auth import auth
